d941b71fe74edc8e137299de70bb688690dde2b3
cecilelu(陆鑫) <cecilelu@tencent.com>
2020-09-24 21:23:34 +0800

    feat: server-side render

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/go-react
 go 1.14
 
 require (
+	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/robertkrimen/otto v0.0.0-20200922221731-ef014fd054ac
+	golang.org/x/net v0.0.0-20200923182212-328152dc79b1 // indirect
+	google.golang.org/appengine v1.6.6
+	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/sourcemap.v1 v1.0.5 // indirect
 )
--- server/render/main.go
+++ server/render/main.go
@@ -2,11 +2,16 @@ package render
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 	"github.com/robertkrimen/otto"
+	"google.golang.org/appengine"
+	"sync"
 )
 
 var templates = template.Must(template.ParseFiles("../../src/public/index.html"))
+
+const jsFile = "../../src/src/server.js"
 
 type IndexPage struct {
 	HTML			template.HTML
@@ -20,12 +25,70 @@ func (fn handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+var callRenderJS = func() (otto.Value, error) {
+	vm := otto.New()
+	renderJS, err := compileRenderJS(vm)
+
+	if err != nil {
+		return otto.NullValue(), err
+	}
+
+	return renderJS.Call(otto.NullValue())
+}
+
 func Init() {
+	if !appengine.IsDevAppServer() {
+		var renderJS otto.Value
+		var once sync.Once
+		callRenderJS = func() (otto.Value, error) {
+			once.Do(func() {
+				vm := otto.New()
+				var err error
+				renderJS, err = compileRenderJS(vm)
+				if err != nil {
+					log.Fatal(err)
+				}
+			})
+
+			return renderJS.Call(otto.NullValue())
+		}
+	}
 	http.Handle("/", handler(handleIndex))
+}
+
+func compileRenderJS(vm *otto.Otto) (otto.Value, error) {
+	var v otto.Value
+	script, err := vm.Compile(jsFile, nil)
+	if err != nil {
+		return v, err
+	}
+
+	v, err = vm.Run(script)
+	if err != nil {
+		return v, err
+	}
+
+	v, err = vm.Get("server")
+	if err != nil {
+		return v, err
+	}
+
+	v, err = v.Object().Get("render")
+	if err != nil {
+		return v, err
+	}
+
+	return v, nil
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) error {
 	var renderHTML string
+
+	if len(r.Header["X-Devserver"]) > 0 {
+		return templates.ExecuteTemplate(w, "../../src/public/index.html", IndexPage {
+			HTML: "",
+		})
+	}
 
 	renderHTML, err := render()
 
@@ -56,35 +119,4 @@ func render() (string, error) {
 	}
 
 	return renderHTML.String(), err
-}
-
-func callRenderJS()(otto.Value, error) {
-	vm := otto.New()
-
-	var v, renderJS otto.Value
-
-	script, err := vm.Compile("../../src/App.js", nil)
-	if err != nil {
-		return v, err
-	}
-
-	v, err = vm.Run(script)
-
-	if err != nil {
-		return v, err
-	}
-
-	v, err = vm.Get("server")
-
-	if err != nil {
-		return v, err
-	}
-
-	v, err = v.Object().Get("render")
-
-	if err != nil {
-		return v, err
-	}
-
-	return renderJS.Call(otto.NullValue())
 }
